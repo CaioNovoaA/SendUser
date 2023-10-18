f1bbe54ae6e6ee2a56976c37a4c9a29605d6f4dc
CaioNovoaA <caio.antunes@zup.com.br>
2023-10-18 02:50:11 -0300

    Não pode ser nulo o email e senha, tratamento simples, criação controller funcional e request

--- src/main/java/com/send/email/controller/UserRequest.java
+++ src/main/java/com/send/email/controller/UserRequest.java
@@ -2,17 +2,34 @@ package com.send.email.controller;
 
 import com.send.email.model.UserTb;
 
-public class UserRequest {
-    private String email;
-    private String senha;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
-    public UserRequest(String email, String senha) {
+public class UserRequest {
+
+    @Email(message = "Email must be a valid email address")
+    private String email;
+
+    @NotBlank(message = "Password cannot be blank")
+    @Size(min = 8, message = "Password must be at least 8 characters")
+    private String password;
+
+    public UserRequest(String email, String password) {
+        if (email == null || email.trim().isEmpty()) {
+            throw new IllegalArgumentException("Email cannot be null or empty");
+        }
+
+        if (password == null || password.trim().isEmpty()) {
+            throw new IllegalArgumentException("Password cannot be null or empty");
+        }
         this.email = email;
-        this.senha = senha;
+        this.password = password;
     }
 
     public UserTb toModel() {
-        return new UserTb(email, senha);
-
+        return new UserTb(email, password);
     }
+
+
 }
--- src/main/java/com/send/email/model/UserTb.java
+++ src/main/java/com/send/email/model/UserTb.java
@@ -4,19 +4,39 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import java.util.Objects;
 
 @Entity
 public class UserTb {
-    private static final long serialVersionUID = 1L;
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long userId;
     private String email;
-    private String senha;
+    private String password;
+    // other variables...
 
-    public UserTb(String email, String senha) {
+
+    public UserTb(String email, String password) {
         this.email = email;
-        this.senha = senha;
+        this.password = password;
+    }
+
+
+    // getters and setters...
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserTb user = (UserTb) o;
+        return Objects.equals(email, user.email) &&
+                Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(email, password);
     }
 }

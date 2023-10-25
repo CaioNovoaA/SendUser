4f777c5e56fd8f471c7f58a835e32554d5d1ad6d
CaioNovoaA <caio.antunes@zup.com.br>
2023-10-25 01:43:46 -0300

    Criação classes necessárias do security e configuração do usuario

--- src/main/java/com/send/email/model/User.java
+++ src/main/java/com/send/email/model/User.java
@@ -1,75 +1,65 @@
 package com.send.email.model;
 
+import com.send.email.enumerador.UserRole;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 @Table(name = "tb_user")
-public class User implements Serializable {
+@EqualsAndHashCode
+public class User implements Serializable, UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
     private String email;
     private String password;
-    // other variables...
-
-@Deprecated
-    public User() {
-    }
-
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-
-
-    public String getPassword() {
-        return password;
-    }
-
+    private UserRole role;
 
     public User(String email, String password) {
         this.email = email;
         this.password = password;
     }
 
-
-    // getters and setters...
-
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return Objects.equals(email, user.email) &&
-                Objects.equals(password, user.password);
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if (this.role == UserRole.ADMIN) return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
+        else return List.of(new SimpleGrantedAuthority("ROLE_USER"));
     }
 
     @Override
-    public int hashCode() {
-        return Objects.hash(email, password);
+    public String getUsername() {
+        return this.email;
     }
 
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
 
     @Override
-    public String toString() {
-        return "UserTb{" +
-                "email='" + email + '\'' +
-                ", password='" + password + '\'' +
-                '}';
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
